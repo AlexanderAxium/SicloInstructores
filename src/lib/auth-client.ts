aebc22bb0d7811c6be3a2d2c94ab023e20eb6d32
Alexander Arancibia <alexander@MacBook-Pro-de-Alexander.local>
2025-10-31 13:08:34 -0500

    fix: fix auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.SITE_URL || "https://myapp.example.com"
+      ? process.env.SITE_URL || "https://siclo.axium.com.pe"
       : "http://localhost:3000",
 });
